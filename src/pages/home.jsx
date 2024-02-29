28a79ca2d0448594846d9e22c351766422284344
mastertony <1510885@iub.edu.bd>
2024-02-29 20:42:34 +0800

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,26 @@
 // Home.js
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
+import { Document, Page } from "react-pdf";
+const pdfUrl = require(`../assets/TestPDFfile.pdf`);
 function Home() {
-  return <button className="button">Button</button>;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <embed id="frPDF" height="100%" width="100%" src={pdfUrl}></embed>
+    </div>
+  );
 }
 
 export default Home;
